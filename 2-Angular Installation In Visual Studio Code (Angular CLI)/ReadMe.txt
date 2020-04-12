9df545746c8a1966e2b418229c3092354180d3e8
iman.rezaeipour <iman.rezaeipour@outlook.com>
2020-04-12 11:48:40 +0430

    first commit

---
+++ 2-Angular Installation In Visual Studio Code (Angular CLI)/ReadMe.txt
@@ -0,0 +1,15 @@
+Using Angular In Visual Studio Code
+
+1- Install Node.js ---> nodejs.org
+
+2- Install "Visual Studio Code" ---> code.visualstudio.com
+
+3- Check Npm Installation ---> [Npm --version]
+
+4- Scafold Angular Structure [npm install -g "@angular/cli"]
+
+5- Create Angular App --> [ng new "ApplicationName"]
+
+6- Running Web Server --> [ng serve]
+
+7- Debugging Client Side Code with "Debugger for Chrome"
---
+++ 22-async programming/Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var task1 = DoSomeThingAsync(1, 2);
+
+			var task2 = DoSomeThingAsync(4, 4);
+
+			Task.WaitAll(task1, task2);
+
+			System.Console.WriteLine(task1.Result);
+
+			System.Console.WriteLine(task2.Result);
+
+			System.Console.ReadLine();
+		}
+
+		private static async Task<int> DoSomeThingAsync(int number1, int number2)
+		{
+			return await Task.Run(() =>
+			{
+				System.Threading.Thread.Sleep(2000);
+				return number1 * number2;
+			});
+		}
+	}
+}
